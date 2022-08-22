d5deed171b064a17557ca52db3d5fe101769f47f
Rodrigo Kennes <rodrigo.kennes@gmail.com>
2022-08-22 18:54:38 -0300

    Estrutura básica

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
